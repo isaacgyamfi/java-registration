5ffb2e7e48b045c6d04510c068b8bdc0858025d9
unknown <frimpongi50@gmail.com>
2019-06-12 10:40:51 +0000

    images added

--- src/home/DbConnection.java
+++ src/home/DbConnection.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-    public String username = ""; //replace with your database user
-    public String password = ""; // replace with your database password
+    public String username = "myuser"; //replace with your database user
+    public String password = "ghana"; // replace with your database password
     public String dbUrl = "jdbc:mysql://localhost:3306/java_registration"; // replace with your database url
 
     Connection conn = DriverManager.getConnection(dbUrl, username, password);
